22f5860e137953ee0ed6821152ded075bfbb4a5b
higorLTA <higor.leonalves@gmailcom>
2023-10-22 15:33:19 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { CreateCommentComponent } from './create-comment/create-comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListComponent,
+    PostDetailComponent,
+    CreatePostComponent,
+    CreateCommentComponent
   ],
   imports: [
     BrowserModule,
